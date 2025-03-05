9773eb1a7f248b69af44ca30456b2ba2c8498e5e
Anup Maurya <anup.maurya484@gmail.com>
2025-03-05 12:57:00 +0530

    env file change

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: process.env.API_URL || "http://localhost:5000",
+    API_URL: process.env.API_URL || "https://wcs-project-manager-server.onrender.com",
 };
 
 // export const environment = {
